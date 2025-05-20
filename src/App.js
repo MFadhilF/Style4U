12ef3cbcdf008e648db66e61e7b7dfad3e168c96
Ridhan Fadhlil <v.ridhan.wafi@adira.co.id>
2025-05-20 22:06:57 +0700

    add path

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DetailProduk from "./views/detailproduk";
 import Home from "./App.jsx"; // ini merujuk ke Home yang ada di App.jsx
+import LebihLanjut from "./views/lebihlanjut.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detailproduk" element={<DetailProduk />} />
+        <Route path="/lebihlanjut" element={<LebihLanjut />} />
       </Routes>
     </Router>
   );
